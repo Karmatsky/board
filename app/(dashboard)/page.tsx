da5a74f7b7418ad8b22b709870c88bf8fdf56132
Savely Karmatsky <s.karmatsky@gmail.com>
2025-06-02 13:08:01 +0500

    fixed page.tsx

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,10 +5,7 @@ import { EmptyOrg } from "./_components/empty-org";
 import { BoardList } from "./_components/board-list";
 
 interface DashboardPageProps {
-    searchParams: {
-        search?: string;
-        favourites?: string;
-    };
+    searchParams?: Record<string, string | string[] | undefined>;
 };
 
 // @ts-ignore
@@ -25,7 +22,10 @@ const DashboardPage = ({
             ) : (
                 <BoardList
                     orgId={organization.id}
-                    query={searchParams}
+                    query={{
+                        search: Array.isArray(searchParams?.search) ? searchParams?.search[0] : searchParams?.search,
+                        favourites: Array.isArray(searchParams?.favourites) ? searchParams?.favourites[0] : searchParams?.favourites,
+                    }}
                 />
             )}
         </div>

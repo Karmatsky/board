e2263c572b8cd61d29054a8338805b7ed4331142
Savely Karmatsky <88971699+Karmatsky@users.noreply.github.com>
2025-06-02 13:21:21 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true, // Полное отключение проверки типов при сборке
+  },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Отключение ESLint проверок
   },
   images: {
     remotePatterns: [
